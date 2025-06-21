b25b9d477c78476923af4c6390ac1598986ef0cd
Samuel Fortes Brasil <samueldbz441@gmail.com>
2025-06-21 17:35:15 -0300

    Exercícios concluídos!

--- aula 19,20-21/script.js
+++ aula 19,20-21/script.js
@@ -3,12 +3,17 @@ const adicionar = document.getElementById('adicionar')
 const cursos = ['Html','Css','Java','Javascript','C++','React','Angular']
 const caixaCursos = document.getElementById('caixa02')
 const remover = document.getElementById('remover')
+const addAntes = document.querySelector('#adicionarA')
+const addDepois = document.querySelector('#adicionarD')
+const nomeCurso = document.getElementById('nomeCurso')
 
-cursos.map((e,i)=>{
+let indice = 0
+
+const criarNovaOpcao = (curso)=>{
     const novoElemento = document.createElement('div')
-    novoElemento.setAttribute('id','c'+i)
+    novoElemento.setAttribute('id','c'+indice)
     novoElemento.setAttribute('class','curso c1')
-    novoElemento.innerHTML = e
+    novoElemento.innerHTML = curso
 
     const comandos = document.createElement('div')
     comandos.setAttribute('class','comandos')
@@ -19,7 +24,14 @@ cursos.map((e,i)=>{
     comandos.appendChild(radBotao)
 
     novoElemento.appendChild(comandos)
+    return novoElemento
+
+}
+
+cursos.map((e,i)=>{
+    const novoElemento = criarNovaOpcao(e)
     caixaCursos.appendChild(novoElemento)
+    indice++
 })
 
 const RadioSelec = () =>{
@@ -52,6 +64,39 @@ remover.addEventListener('click',(e)=>{
 
 
 })
+
+addAntes.addEventListener('click',(e)=>{
+    const rdSelc = RadioSelec()
+    if(rdSelc == undefined){
+        alert('Selecione uma opção antes de prosseguir')
+    }else{
+        if(nomeCurso.value ==''){
+            alert('Você deve digitar algo antes de adicionar!')
+        }else{
+            const cursoSelecionado = rdSelc.parentNode.parentNode
+            const novaOpcao = criarNovaOpcao(nomeCurso.value)
+            caixaCursos.insertBefore(novaOpcao,cursoSelecionado)
+        }
+    }
+
+})
+
+addDepois.addEventListener('click',(e)=>{
+        const rdSelc = RadioSelec()
+    if(rdSelc == undefined){
+        alert('Selecione uma opção antes de prosseguir')
+    }else{
+        if(nomeCurso.value == ''){
+            alert('Você deve digitar algo antes de adicionar!')
+        }else{
+            const cursoSelecionado = rdSelc.parentNode.parentNode
+            const novaOpcao = criarNovaOpcao(nomeCurso.value)
+            caixaCursos.insertBefore(novaOpcao,cursoSelecionado.nextSibling)
+        }
+    }
+
+})
+
 
 
 
