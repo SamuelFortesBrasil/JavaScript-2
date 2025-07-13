e40e40757bd3d7d4b178b88a1eb32702c7f6070f
Samuel Fortes Brasil <samueldbz441@gmail.com>
2025-07-13 15:02:23 -0300

    Aprendendo Sobre Json

---
+++ aula 35/script.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome: 'Pessoa',
+    idade: 17,
+    linguagem: "JavaScript",
+    Extras: "Html & Css"
+}
+
+const s_json = JSON.stringify(pessoa)
+
+console.log(pessoa)
+console.log(s_json)
