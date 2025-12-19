57c7b887be06d2e55cde605638d7b1a696c79879
Samuel Fortes Brasil <samueldbz441@gmail.com>
2025-12-19 16:38:55 -0300

    Criação de Novas pastas de Exercício

---
+++ aula 52/script.js
@@ -0,0 +1,10 @@
+let nome = 'Samuel'
+let sobrenome = ' Fortes Brasil'
+
+console.log(nome.charAt(0))
+console.log(nome.charCodeAt(0))
+console.log(nome.concat(sobrenome))
+console.log(nome.indexOf('u'))
+console.log(nome.replace("S","Ç"))
+console.log(nome.concat(sobrenome).replaceAll('a','á'))
+console.log(nome.slice(0,4))
