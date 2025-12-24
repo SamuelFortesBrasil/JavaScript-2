b980e2223d004755f5ad7f3c14fa08ce021e3a12
Samuel Fortes Brasil <samueldbz441@gmail.com>
2025-12-24 15:32:31 -0300

    Substituição segura no Regex de Emails

--- Regex/validarEmail.js
+++ Regex/validarEmail.js
@@ -2,7 +2,7 @@ const regex = /^[^\s\W][\w.@!$-_]+\@[\w]+\.[\w]+\.?[\w]+$/gmi
 //const objRegex = new RegExp(/^[^\s\W][\w.@!$-_]+\@[\w]+\.[\w]+\.?[\w]+$/gmi)
 
 
-const Email = ['samueldbz441@gmail.com','marcos.dbz441@gmail.com','exemple123@exemple.com.br','.doc@document.cd',' aspwoq@exemple.com']
+const Email = ['Yamatodyz123@gmail.com','mava.125@gmail.com','exemple123@exemple.com.br','.doc@document.cd',' aspwoq@exemple.com']
 
 Email.map((e)=>{
    let testeDeValidacao = e.match(regex)
